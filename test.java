54666480dd0d7c18275a1ce86a6b6702f3a6671c
surinder0322@gmail.com <sunny@Sunnys-Laptop.local>
2023-07-13 21:03:10 -0400

    added sum

--- test.java
+++ test.java
@@ -3,5 +3,10 @@ public class TestCode {
 System.out.print("Surinder");
         System.out.println("Raman");
         System.out.println("Surinder2");
+      int x = 10;
+      int y = 12;
+      int sum = x+ y;
+        System.out.println(sum);
+
     }
 }
